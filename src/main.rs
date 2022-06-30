7c6ebdf51925c8b5aa871554e09556d6e38ee0a8
yinguobing <yinguobing@gmail.com>
2022-06-30 13:52:39 +0800

    encode the input image into base64 string

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use opencv::{prelude::*, videoio, imgcodecs};
+use base64::encode;
+use opencv::{imgcodecs, prelude::*, videoio};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
@@ -15,9 +16,7 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/status", get(status))
-        // `POST /users` goes to `create_user`
+        .route("/status", post(status))
         .route("/capture", post(capture));
 
     // run our app with hyper
@@ -35,28 +34,47 @@ async fn status() -> &'static str {
 }
 
 async fn capture(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
+    // this argument tells axum to parse the request body as JSON into a `CaptureRequest` type
     Json(payload): Json<CaptureRequest>,
 ) -> impl IntoResponse {
-    println!("{}", payload.rtsp);
+    tracing::debug!("Opening {}", payload.rtsp);
+    let mut err_msg: String = "".to_string();
+    let mut img_b64: String = "".to_string();
+
     if let Ok(mut cam) = videoio::VideoCapture::from_file(&payload.rtsp, videoio::CAP_ANY) {
         let mut frame = Mat::default();
+        let mut retry_count = 0;
         loop {
             if let Ok(result) = cam.read(&mut frame) {
                 if result == true {
                     let params: opencv::core::Vector<i32> = opencv::core::Vector::new();
-                    let _r = imgcodecs::imwrite("test.jpg", &frame, &params);
-                    break;
-                };
-            };
+                    let mut buf: opencv::core::Vector<u8> = opencv::core::Vector::new();
+                    let r = imgcodecs::imencode(".jpg", &frame, &mut buf, &params);
+                    match r {
+                        Ok(true) => {
+                            img_b64 = encode(buf);
+                            break;
+                        }
+                        Ok(false) => println!("Failed to encode image."),
+                        Err(e) => println!("{}", e),
+                    };
+                } else {
+                    retry_count += 1;
+                }
+            } else {
+                retry_count += 1;
+            }
+            if retry_count > 25 * 5 {
+                err_msg = "Time out reading video stream.".to_string();
+                break;
+            }
         }
-    };
+    } else {
+        err_msg = "Failed to open video stream.".to_string();
+    }
+
     // this will be converted into a JSON response with a status code of `201 Created`
-    let cap_result = CaptureResult {
-        id: 0,
-        img_b64: "SOME BASE 65 STRING".to_string(),
-    };
+    let cap_result = CaptureResult { img_b64, err_msg };
     (StatusCode::CREATED, Json(cap_result))
 }
 
@@ -64,11 +82,13 @@ async fn capture(
 #[derive(Deserialize)]
 struct CaptureRequest {
     rtsp: String,
+    max_width: u32,
+    max_height: u32,
 }
 
 // the output to our `capture` handler
 #[derive(Serialize)]
 struct CaptureResult {
-    id: u64,
     img_b64: String,
+    err_msg: String,
 }
