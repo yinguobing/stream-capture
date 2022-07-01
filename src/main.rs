c4cb9e231b225164abbb5b6fad3a7e01a8f52161
yinguobing <yinguobing@gmail.com>
2022-07-01 16:41:12 +0800

    监听全部地址请求

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() {
         .route("/capture", post(capture));
 
     // run our app with hyper
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
